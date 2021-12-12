b82b0305ca1d46f5f1a3b6ab76721bb1b570e080
Brandon Isaac Fonseca <isaac99.bf@gmail.com>
2021-12-12 17:40:29 -0600

    addding Home view and Login view as initial views

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import react from "react";
+
+const Home = ()=>{
+
+    return(
+        <div>
+            <h1>{"Hello from home"}</h1>
+        </div>
+    )
+}
+
+
+export default Home;
---
+++ src/pages/Login.js
@@ -0,0 +1,23 @@
+import react from "react";
+
+import Form from "../components/Form"
+import RoundedTextField from "../components/RoundedTextField";
+
+import "./styles/Login.css"
+
+const fields = [
+    { type: "text"},
+    {}
+]
+const Login = () => {
+
+    return (
+        <div id="container-login">
+            <Form>
+            </Form>
+        </div>
+    );
+}
+
+
+export default Login;
