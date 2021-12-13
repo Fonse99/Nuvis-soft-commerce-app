6283330a24467cf1d6aa731bd51883d72cb45551
Brandon Isaac Fonseca <isaac99.bf@gmail.com>
2021-12-12 22:29:06 -0600

    adding new page and their respective route

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,9 @@
 import react from "react";
 
 import Form from "../components/Form"
-import RoundedTextField from "../components/RoundedTextField";
 
 import "./styles/Login.css"
 
-const fields = [
-    { type: "text"},
-    {}
-]
 const Login = () => {
 
     return (
---
+++ src/pages/PointOfSale.js
@@ -0,0 +1,21 @@
+import react from "react";
+import Header from "../components/base/Header";
+import Table from "../components/Table";
+
+const PointOfSale = () => {
+
+    return (
+
+        <div>
+            <Header />
+
+            <div>
+                <Table />
+            </div>
+        </div>
+
+    )
+
+}
+
+export default PointOfSale;
